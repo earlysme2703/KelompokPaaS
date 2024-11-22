6b122cff8a640efbc8df3763d235e3069fe85a15
testing437 <zakiherex@gmail.com>
2024-11-22 17:34:16 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/login', function () {
-    return view('login');
-})->name('login');
+// routes/web.php
+Route::get('/login', [AuthController::class, 'login'])->name('login');
+Route::get('/register', [AuthController::class, 'register'])->name('register');
 
-Route::get('/register', function () {
-    return view('register');
-})->name('register');
+
+// Route::get('/login', function () {
+//     return view('login');
+// })->name('login');
+
+// Route::get('/register', function () {
+//     return view('register');
+// })->name('register');
